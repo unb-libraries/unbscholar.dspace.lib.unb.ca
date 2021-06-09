1f182b47d78d5df8a10afe93c9da1c65e474e5de
Jacob Sanford <jsanford@unb.ca>
2021-06-09 08:12:19 -0300

    Avoid using multiline sed in tokenizing

--- build/scripts/add_xforward_tomcat.sh
+++ build/scripts/add_xforward_tomcat.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env sh
 #
 # Adds incoming X-Forwarded-Proto and X-Forwarded-For values to tomcat app headers.
-sed -i 's|
-  </Host>|
-    <Valve className="org.apache.catalina.valves.RemoteIpValve"
-      remoteIpHeader="X-Forwarded-For"
-      protocolHeader="X-Forwarded-Proto"/>
-  </Host>|'
-  /usr/local/tomcat/conf/server.xml
+sed -i 's|</Host>|<Valve className="org.apache.catalina.valves.RemoteIpValve" remoteIpHeader="X-Forwarded-For" protocolHeader="X-Forwarded-Proto"/></Host>|' /usr/local/tomcat/conf/server.xml
