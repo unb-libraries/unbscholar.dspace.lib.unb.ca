64d0175322110e55384919e5c0b1ddc826762a70
Jacob Sanford <jsanford@unb.ca>
2023-01-24 10:44:42 -0400

    Compare strings with single equals in bash

--- build/scripts/pre-init.d/75_enable_shib_auth.sh
+++ build/scripts/pre-init.d/75_enable_shib_auth.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-if [ "$DEPLOY_ENV" == "prod" ]; then
+if [ "$DEPLOY_ENV" = "prod" ]; then
   echo "Enabling Shibboleth Authentication."
   printf "plugin.sequence.org.dspace.authenticate.AuthenticationMethod = org.dspace.authenticate.ShibAuthentication\n" >> "$DSPACE_INSTALL/config/"
 fi
