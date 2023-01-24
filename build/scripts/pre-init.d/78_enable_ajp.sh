3277bd67468c062c4dc3feefe5002842a9903a18
Jacob Sanford <jsanford@unb.ca>
2023-01-24 12:04:16 -0400

    Do not require secret for AJP connections

--- build/scripts/pre-init.d/78_enable_ajp.sh
+++ build/scripts/pre-init.d/78_enable_ajp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" = "prod" ]; then
   echo "Enabling AJP connections."
-  sed -i 's|<Connector port="8080"|<Connector protocol="AJP/1.3" address="0.0.0.0" port="8009" redirectPort="8443"/><Connector port="8080"|' /usr/local/tomcat/conf/server.xml
+  sed -i 's|<Connector port="8080"|<Connector protocol="AJP/1.3" address="0.0.0.0" port="8009" redirectPort="8443" secretRequired="false" /><Connector port="8080"|' /usr/local/tomcat/conf/server.xml
 fi
