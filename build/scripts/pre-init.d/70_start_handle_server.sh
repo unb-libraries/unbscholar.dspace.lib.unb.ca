777d6fc40f5754eef74dcd0e34679b42843743d7
Jacob Sanford <jsanford@unb.ca>
2023-03-01 11:43:28 -0400

    Enable handle server on startup

--- build/scripts/pre-init.d/70_start_handle_server.sh
+++ build/scripts/pre-init.d/70_start_handle_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" = "prod" ]; then
   echo "Starting handle server..."
-  # ${DSPACE_INSTALL}/bin/start-handle-server
+  ${DSPACE_INSTALL}/bin/start-handle-server
 fi
