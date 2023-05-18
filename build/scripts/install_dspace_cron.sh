ea5372b254c96b062f8176aae9ef99262538816c
Jacob Sanford <jsanford@unb.ca>
2023-05-18 15:02:58 -0300

    Copy cron files after installing cron

--- build/scripts/install_dspace_cron.sh
+++ build/scripts/install_dspace_cron.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
-$RSYNC_MOVE /scripts/dspace_cron /etc/cron.d/dspace_cron
-chmod 644 /etc/cron.d/dspace_cron
-
 # Install and enable cron.
 apt-get update
 apt-get install cron
 rm -rf /var/lib/apt/lists/*
+
+mkdir -p /etc/cron.d/
+$RSYNC_MOVE /scripts/dspace_cron /etc/cron.d/dspace_cron
+chmod 644 /etc/cron.d/dspace_cron
+
 service cron start
