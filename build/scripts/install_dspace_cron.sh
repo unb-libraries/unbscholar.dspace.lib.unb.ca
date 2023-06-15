026390ffdb9c10858121b0de315a45ab3503820c
Jacob Sanford <jsanford@unb.ca>
2023-06-15 13:32:00 -0300

    Use /etc/crontab for dspace cron

--- build/scripts/install_dspace_cron.sh
+++ build/scripts/install_dspace_cron.sh
@@ -3,7 +3,4 @@
 apt-get update
 apt-get install cron
 rm -rf /var/lib/apt/lists/*
-
-mkdir -p /etc/cron.d/
-$RSYNC_MOVE /scripts/dspace_cron /etc/cron.d/dspace_cron
-chmod 644 /etc/cron.d/dspace_cron
+cat /scripts/dspace_cron >> /etc/crontab
